ae3c3008cbf8c43d8770351596c076a361063ead
Khairuddin Nasty <56571284+khairuddin-n@users.noreply.github.com>
2025-01-07 12:05:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import random
 import numpy as np
 import tensorflow as tf
 from config import Config
-from modular_training_system import ModularTrainingSystem
+from model_training_pipeline import ModelTrainingPipeline
+from tensorflow.keras.models import load_model
 from visualizer import Visualizer
 import warnings
 
@@ -18,18 +19,35 @@ def set_seeds(seed=42):
 
 if __name__ == "__main__":
     set_seeds(Config.SEED)
-    
-    system = ModularTrainingSystem()
-    
+
+    system = ModelTrainingPipeline()
+
     # Train specific configurations
-    system.train_single_configuration('he_normal', 'relu')
-    
-    
+    # system.train_single_configuration('he_normal', 'relu')
+    # system.train_single_configuration('he_normal', 'leaky_relu')
+    # ...
+
     # Load all results and analyze
-    all_results = ModularTrainingSystem.load_results()
-    
+    all_results = ModelTrainingPipeline.load_results()
+
+    # Plot individual loss and accuracy for all configurations
+    # Visualizer.plot_convergence(all_results, save_dir="plots")
+
+    model_path = os.path.join(Config.MODEL_DIR, "he_normal_relu.keras")
+    model = load_model(model_path)
+
+    # Performance comparison
     performance_table = Visualizer.compare_performance(all_results)
     print("Performance Comparison:")
     print(performance_table)
-    
-    Visualizer.check_generalization(all_results, performance_table)
+
+    # Save performance table to an Excel file
+    # performance_table_file = "performance_comparison.xlsx"
+    # performance_table.to_excel(performance_table_file, index=False)
+    # print(f"Performance table saved to {performance_table_file}")
+
+    # Visualisasi confusion matrix
+    Visualizer.plot_confusion_matrix(
+        model, 
+        system.test_generator
+    )
